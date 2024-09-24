178a6d348e6861fa1d00792720430426ed94d5a0
hardikh05 <148034772+hardikh05@users.noreply.github.com>
2024-09-24 16:23:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
  import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
- import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
